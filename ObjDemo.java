edf18bbcf410e9a3dfa0936fd6db699e22802f0d
sharon-afk <149856702+sharon-afk@users.noreply.github.com>
2025-02-17 11:15:15 +0530

    Create ObjDemo.java

---
+++ ObjDemo.java
@@ -0,0 +1,12 @@
+public class ObjDemo
+{
+    void msg()
+    {
+        System.out.println("Classes & Objects");
+    }
+    public static void main(String args[])
+    {
+        ObjDemo ob=new ObjDemo();
+        ob.msg();
+    }
+}
