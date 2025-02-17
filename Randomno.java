80b7767cddf6b145b4e90bc82e4199adb40d334a
sharon-afk <149856702+sharon-afk@users.noreply.github.com>
2025-02-17 11:15:49 +0530

    Create Randomno.java

---
+++ Randomno.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+public class Randomno
+{
+    public static void main(String args[])
+    {
+        System.out.println("1st random number:"+Math.random());
+        System.out.println("2nd random number:"+Math.random());
+    }
+}
