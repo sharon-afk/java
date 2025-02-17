bef08c810ecce2119e914c6d7e1a2b18d8ef8fd1
sharon-afk <149856702+sharon-afk@users.noreply.github.com>
2025-02-17 10:59:46 +0530

    Create Details.java

---
+++ Details.java
@@ -0,0 +1,10 @@
+public class Details
+{
+    public static void main(String args[])
+    {
+        int age=10;
+        String name="SHARON";
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+    }
+}
