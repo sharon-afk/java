730bb541d773a87dd5718625435751fa5682cbd8
sharon-afk <149856702+sharon-afk@users.noreply.github.com>
2025-02-17 11:00:25 +0530

    Create Details2.java

---
+++ Details2.java
@@ -0,0 +1,10 @@
+public class Details2
+{
+    public static void main(String args[])
+    {
+        int rollno=39;
+        String course="MCA";
+        System.out.println("Course:"+course);
+        System.out.println("Rollno:"+rollno);
+    }
+}
