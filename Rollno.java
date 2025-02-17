c3615b7890ed5117307ec9b6f232d4d0b8187d31
sharon-afk <149856702+sharon-afk@users.noreply.github.com>
2025-02-17 11:17:49 +0530

    Create Rollno.java

---
+++ Rollno.java
@@ -0,0 +1,12 @@
+public class Rollno
+{
+    void getinput(int r1)
+    {
+        System.out.println("Rollno="+r1);
+    }
+    public static void main(String args[])
+    {
+        Rollno r=new Rollno();
+        r.getinput(39);
+    }
+}
