e6d3436c48d721a84ec306cebac8d6e8ae26d0e1
sharon-afk <149856702+sharon-afk@users.noreply.github.com>
2025-02-17 11:13:34 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,12 @@
+public class Employee
+{
+    void displaymsg()
+    {
+        System.out.println("Employees & Details");
+    }
+    public static void main(String args[])
+    {
+        Employee emp=new Employee();
+        emp.displaymsg();
+    }
+}
